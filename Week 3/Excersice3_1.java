7209225583daba65ff170153fe635020d976ae6d
Kiruthika <66116348+Kiruthi-1312@users.noreply.github.com>
2021-04-06 17:48:43 +0530

    Create Excersice3_1.java

---
+++ Week 3/Excersice3_1.java
@@ -0,0 +1,32 @@
+//This program is related to the generation of Fibonacci numbers.
+
+// For example: 0,1, 1,2, 3,5, 8, 13,… is a Fibonacci sequence where 13 is the 8th Fibonacci number.
+
+import java.util.*;
+
+public class Fibonacci {
+
+	static int fib(int n)
+	{
+		if(n==1)
+		{
+			return 0;
+		}
+		else if(n==2)
+		{
+			return 1;
+		}
+		else
+		{
+			return fib(n-1)+fib(n-2);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println(fib(n));
+	}
+
+}
