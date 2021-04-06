2a6d9d7065ebbea283f56d7a7e72fb790da0e326
Kiruthika <66116348+Kiruthi-1312@users.noreply.github.com>
2021-04-06 18:20:52 +0530

    Create Question42.java

---
+++ Week 4/Question42.java
@@ -0,0 +1,17 @@
+//Complete the code segment to print the current year. Your code should compile successfully.
+
+//Note: In this program, you are not allowed to use any import statement. Use should use predefined class Calendar defined in java.util package.
+
+public class Question42 { 
+	public static void main(String args[]){
+		int year; 
+java.util.Calendar current;
+    current = java.util.Calendar.getInstance();
+
+year = current.get(current.YEAR);
+
+System.out.println("Current Year: "+year);
+//int month = current.get(current.MONTH);
+System.out.print("sum: "+ 5);
+  }  
+}
