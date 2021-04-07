b00b4199b8fef6a45031b0da861109711bf8c459
Kiruthika <66116348+Kiruthi-1312@users.noreply.github.com>
2021-04-07 13:39:33 +0530

    Create Question5_4.java

---
+++ Week 5/Question5_4.java
@@ -0,0 +1,25 @@
+// In the following program, an array of integer data to be initialized. 
+// During the initialization, if a user enters a value other than integer value, then it will throw InputMismatchException exception.
+// On the occurrence of such an exception, your program should print “You entered bad data.” If there is no such exception it will print the total sum of the array.
+
+import java.util.*;
+public class Question5_4{
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in); 
+      int length = sc.nextInt(); 
+     
+      int[] name = new int[length];
+      int sum=0;
+ try{
+       for(int i=0;i<length;i++){  
+          int userInput=sc.nextInt();
+          name[i] = userInput;
+          sum=sum+name[i]; 
+          } 
+        System.out.println(sum);
+        }
+       catch(InputMismatchException e) {
+        System.out.println("You entered bad data.");
+        }
+ }
+}
