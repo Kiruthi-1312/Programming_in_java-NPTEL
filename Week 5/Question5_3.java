bd060202bf46a543112f785ab61e7671538dbd2e
Kiruthika <66116348+Kiruthi-1312@users.noreply.github.com>
2021-04-07 13:27:15 +0530

    Create Question5_3.java

---
+++ Week 5/Question5_3.java
@@ -0,0 +1,26 @@
+// Complete the code segment to catch the ArithmeticException in the following, if any.
+// On the occurrence of such an exception,your program should print “Exception caught: Division by zero.”
+// If there is no such exception, it will print the result of division operation on two integer values
+
+import java.util.Scanner;
+  
+  public class Question5_3 {
+  public static void main(String[] args) { 
+      int a, b;
+      Scanner input = new Scanner(System.in);
+
+      int result;  
+       a = input.nextInt();
+       b = input.nextInt();
+  
+         try {
+              result = a/b;
+              System.out.println(result);
+     	     }
+          
+          catch (ArithmeticException e) {
+             System.out.println("Exception caught: Division by zero.");
+          }
+
+   }
+}
