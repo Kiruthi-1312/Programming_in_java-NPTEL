cf5ac03d581eb52cf9e855e3776e56a7f49eeae7
Kiruthika <66116348+Kiruthi-1312@users.noreply.github.com>
2021-04-08 17:23:15 +0530

    Create Question65.java

---
+++ Week 6/Question65.java
@@ -0,0 +1,35 @@
+//Add necessary codes to print the following:
+//
+//-----------------OUTPUT-------------------
+//
+//Name of thread 't1':Thread-0
+//Name of thread 't2':Thread-1
+//New name of thread 't1':Week 6 Assignment Q5
+//New name of thread 't2':Week 6 Assignment Q5 New
+
+public class Question65 extends Thread{  
+  public void run(){  
+      
+  }  
+ public static void main(String args[]){  
+    Question65 t1=new Question65();  
+    System.out.println("Name of thread 't1':"+ t1.getName());  
+
+Question65 t2=new Question65();  
+    System.out.println("Name of thread 't2':"+ t2.getName());
+
+
+  t1.start();  
+
+  t1.setName("Week 6 Assignment Q5");  
+
+
+  t2.start();  
+
+  t2.setName("Week 6 Assignment Q5 New");  
+
+   System.out.println("New name of thread 't1':"+ t1.getName()); 
+   System.out.println("New name of thread 't2':"+ t2.getName());
+ 
+ }  
+}
